ba1212cf07fd7795c53f5941cc507de4261c3eb0
KeijiNikolaevich <90941318+KeijiNikolaevich@users.noreply.github.com>
2023-11-07 12:24:25 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Name/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char N[25];
+
+  cout << "Name: ";
+  cin >> N;
+  cout << "Hello " << N << ", nice to meet you!" << endl;
+  return 0;
+}
